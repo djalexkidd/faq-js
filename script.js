7ecc647f48c73da330c7a0a5b25bdd8860d7b0d9
djalexkidd <alexpyr@outlook.fr>
2022-02-12 22:44:25 +0100

    haha tile go green

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const question = document.querySelectorAll('.question');
+const questions = document.querySelectorAll('.question');
 const arrowDown = document.querySelectorAll('.arrow-down');
 const answers = document.querySelectorAll('.answer');
 
-for(let i = 0; i < question.length; i++) {
+for(let i = 0; i < questions.length; i++){
     answers[i].style.display = 'none';
 
-    question[i].addEventListener('click', function() {
-        // (answers[i].style.display === 'none') ? answers[i].style.display = "block" : answers[i].style.display = "none";
+    questions[i].addEventListener('click', function(){
+        // (answers[i].style.display ==='none') ? answers[i].style.display = "block" : answers[i].style.display = "none";
 
-        if(answers[i].style.display === 'none') {
+        if(answers[i].style.display === 'none'){
             answers[i].style.display = 'block';
             arrowDown[i].style.transform = 'rotate(180deg)';
-        }
-        else {
+        } else {
             answers[i].style.display = "none";
-            arrowDown[i].style.transform = "transform(0deg)";
+            arrowDown[i].style.transform = 'rotate(0deg)';
         }
     })
 }
